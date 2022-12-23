11243620a7a80c4b9568b9a3b5e16b23ed375de2
anassalem1 <120042435+anassalem1@users.noreply.github.com>
2022-12-23 19:01:16 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,49 +1,28 @@
 'use strict';
-/* Vad ska vi göra här? */
-const bookList = [
-  {
-    id: 1,
-    author: 'Charles Dickens',
-    title: 'Oliver Twist'
-  },
-  {
-    id: 2,
-    author: 'William Shakespear',
-    title: 'Hamlet'
-  }
-];
-const searchInput = null;
 
-function handleKeyPress(input) {
-  /*  Ta emot/läsa av värdet i inputfältet. 
-      Skicka värdet till searchBooks
-      searchBooks returnerar en filtrerad lista
-      Filtrerade listan skickas till renderBookList
-  */
-  searchBooks(input);
+let bookList = [];
+
+window.addEventListener('load', () => {
+  getAll().then((apiBooks) => (bookList = apiBooks));
+});
+
+searchField.addEventListener('keyup', (e) =>
+  renderBookList(
+    bookList.filter(({ title, author }) => {
+      const searchTerm = e.target.value.toLowerCase();
+      return (
+        title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        author.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+    })
+  )
+);
+
+function renderBookList(bookList) {
+  const existingElement = document.querySelector('.book-list');
+
+  const root = document.getElementById('root');
+
+  existingElement && root.removeChild(existingElement);
+  bookList.length > 0 && searchField.value && root.insertAdjacentHTML('beforeend', BookList(bookList));
 }
-
-function searchBooks(searchTerm) {
-  /*  Loopa igenom bookList
-      För varje varv i loopen, ta det aktuella elementet (boken)
-      Jämför titeln med söktermen
-      Om söktermen finns någonstans i titeln, lägg till elementet i ny lista (filteredList)
-      Returnerar filteredList eller anropar renderBookList?
-
-    */
-  const filteredList = [];
-  for (let i = 0; i < bookList.length; i++) {
-    const title = bookList[i].title.toLowerCase();
-    if (title.indexOf(searchTerm.toLowerCase()) >= 0) {
-      filteredList.push(bookList[0]);
-    }
-  }
-  renderBookList(filteredList);
-}
-
-function renderBookList(list) {
-  /* Element i HTML-listan visas/döljs beroende på listans innehåll.  */
-  console.log(list);
-}
-
-handleKeyPress('e');
